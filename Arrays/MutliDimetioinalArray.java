345f640e7be14661bfecedd525317d3b7b06bbc8
shravan-jcode <shravanjadhav019@gmail.com>
2026-06-14 22:49:25 +0530

    multi dimentional aray

---
+++ Arrays/MutliDimetioinalArray.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class MutliDimetioinalArray {
+    public static void main(String args[]){
+        int num[][] =new int [3][4];
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                num[i][j]=(int)(Math.random()*10);
+            }
+        }
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                System.out.print(num[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        //enhanced array
+        for (int[] n : num) {
+            for(int a:n){
+                System.out.print(a+" ");
+            }
+            System.out.println();
+            
+        }
+    }
+}
