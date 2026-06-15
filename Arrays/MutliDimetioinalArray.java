037ecc468fa50b1aaf37e2179f07ab042e87d8a8
shravan-jcode <shravanjadhav019@gmail.com>
2026-06-15 22:08:50 +0530

    jagged array/ 3d Array

--- Arrays/MutliDimetioinalArray.java
+++ Arrays/MutliDimetioinalArray.java
@@ -2,16 +2,19 @@ package Arrays;
 
 public class MutliDimetioinalArray {
     public static void main(String args[]){
+
+        //2d Array
+        System.out.println("2D Array");
         int num[][] =new int [3][4];
 
-        for(int i=0;i<3;i++){
-            for(int j=0;j<4;j++){
+        for(int i=0;i<num.length;i++){
+            for(int j=0;j<num[i].length;j++){
                 num[i][j]=(int)(Math.random()*10);
             }
         }
 
-        for(int i=0;i<3;i++){
-            for(int j=0;j<4;j++){
+        for(int i=0;i<num.length;i++){
+            for(int j=0;j<num[i].length;j++){
                 System.out.print(num[i][j]+" ");
             }
             System.out.println();
@@ -26,5 +29,54 @@ public class MutliDimetioinalArray {
             System.out.println();
             
         }
+
+
+        //jagged Array
+           System.out.println();
+        System.out.println("Jageed Array");
+        int[][] jaggedArr =new int[3][];
+        jaggedArr[0]=new int[2];
+        jaggedArr[1]=new int[1];
+        jaggedArr[2]=new int[3];
+
+        for(int i=0;i<jaggedArr.length;i++){
+            for (int j=0;j<jaggedArr[i].length;j++){
+                jaggedArr[i][j]=(int)(Math.random()*10);
+            }
+        }
+
+     
+        for (int[] jg : jaggedArr) {
+            for (int a : jg) {
+                System.out.print(a);
+                
+            }
+            System.out.println();
+        }
+
+        //3D Array
+        System.out.println();
+        System.out.println("3D Array");
+        int[][][] threeD =new int [2][3][4];
+
+        for(int i=0;i<threeD.length;i++){
+            for(int j=0;j<threeD[i].length;j++){
+                for(int k=0;k<threeD[i][j].length;k++){
+                    threeD[i][j][k]=(int)(Math.random()*10);
+                }
+            }
+        }
+
+        for (int[][] layer : threeD) {
+            for (int[] row : layer) {
+                for (int x  : row) {
+                    System.out.print(x +" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+        
     }
 }
---
+++ Arrays/ThreeD.java
@@ -0,0 +1,65 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ThreeD {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number to find: ");
+        int num = sc.nextInt();
+
+        boolean found = false;
+
+        // 3D Array: 2 layers, 3 rows, 4 columns
+        int[][][] threeD = new int[2][3][4];
+
+        // Fill the array with random numbers (0-9)
+        for (int i = 0; i < threeD.length; i++) {
+            for (int j = 0; j < threeD[i].length; j++) {
+                for (int k = 0; k < threeD[i][j].length; k++) {
+                    threeD[i][j][k] = (int) (Math.random() * 10);
+                }
+            }
+        }
+
+        // Print the array
+        System.out.println("\n3D Array:");
+
+        for (int i = 0; i < threeD.length; i++) {
+            System.out.println("Layer " + i + ":");
+
+            for (int j = 0; j < threeD[i].length; j++) {
+                for (int k = 0; k < threeD[i][j].length; k++) {
+                    System.out.print(threeD[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println();
+        }
+
+        // Search the number
+        outer:
+        for (int[][] layer : threeD) {
+            for (int[] row : layer) {
+                for (int x : row) {
+                    if (x == num) {
+                        found = true;
+                        break outer; // Exit all loops
+                    }
+                }
+            }
+        }
+
+        // Result
+        if (found) {
+            System.out.println(num + " exists in the array.");
+        } else {
+            System.out.println(num + " doesn't exist in the array.");
+        }
+
+        sc.close();
+    }
+}
